eddaafcdbe46d296e63096b6429e6c6e103ee08b
Larryking007 <ajikobilanre007@gmail.com>
2022-07-01 18:10:19 +0100

    name change

--- routes/todoRoute.js
+++ routes/todoRoute.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const router = express.Router();
-const controller = require('../controllers/flightController');
+const controller = require('../controllers/todoController');
 
 router.get('/', controller.example)
 
